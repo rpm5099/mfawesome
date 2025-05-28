987a0b74cbfa9b8307ceb28a71ad82b712cd928d
Duke McCoy <rpm5099@gmail.com>
2025-05-27 21:55:28 -0400

    disable sorting of encrypted secrets

--- mfawesome/config.py
+++ mfawesome/config.py
@@ -312,7 +312,6 @@ def Readyaml(fname: Path | str) -> str:
 
 
 def SortSecrets(secrets):
-    printdbg(f"{secrets=}")
     if isinstance(secrets, list):
         return secrets
     return OrderedDict(sorted(secrets.items(), key=lambda x: x[0].casefold()))
